f1ab6f8e0530604cf755941058613dcd013d5490
He Ia <He Ia@172.16.86.239>
2014-01-14 11:21:46 +0100

    Correction du fichier App

--- tpforma/src/main/java/fr/gphy/m2/keuv/tpforma/App.java
+++ tpforma/src/main/java/fr/gphy/m2/keuv/tpforma/App.java
@@ -1,12 +1,14 @@
 package fr.gphy.m2.keuv.tpforma;
 
+import java.io.IOException;
+
 /**
  * Hello world!
  *
  */
 public class App {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         System.out.println("-Bonjour Marion");
         System.out.println("-Bonjour Heia");
         System.out.println("-Je déclare la guerre!");
