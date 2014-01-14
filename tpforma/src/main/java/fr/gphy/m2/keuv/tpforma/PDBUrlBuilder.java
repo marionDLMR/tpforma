cacd11bb2fc14f6057157645e98bb32fc5fc264a
He Ia <He Ia@172.16.86.239>
2014-01-14 11:04:35 +0100

    Correction du fichier PDBUrlBuilder - partie lecture

--- tpforma/src/main/java/fr/gphy/m2/keuv/tpforma/PDBUrlBuilder.java
+++ tpforma/src/main/java/fr/gphy/m2/keuv/tpforma/PDBUrlBuilder.java
@@ -20,7 +20,15 @@ public class PDBUrlBuilder {
        URL url = new URL("http://www.uniprot.org/uniprot/" + uniprotId + ".txt");
    BufferedReader in = new BufferedReader(new InputStreamReader(url.openStream()));
    //TODO : lire le fichier
-   return null;
+   String line;
+   while((line = in.readLine()) != null){
+   if(line.matches("^DR\\s+PDB;.+$")){
+   in.close();
+   }
+   return new URL("http://www.ebi.ac.uk/pdbe/entry-files/pdb" + line.substring(10,14).toLowerCase() + ".ent");
+   }
+   in.close();
+       return null;
    }
 }
 
