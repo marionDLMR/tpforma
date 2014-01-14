c4d21267ce0cd1860bc54a91bee3ce8ac4e5b14f
He Ia <He Ia@172.16.86.239>
2014-01-14 10:46:46 +0100

    Correction du fichier PDBUrlBuilder

--- tpforma/src/main/java/fr/gphy/m2/keuv/tpforma/PDBUrlBuilder.java
+++ tpforma/src/main/java/fr/gphy/m2/keuv/tpforma/PDBUrlBuilder.java
@@ -1,7 +1,9 @@
 package fr.gphy.m2.keuv.tpforma;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
+import java.net.URL;
 
 /*
  * To change this template, choose Tools | Templates
